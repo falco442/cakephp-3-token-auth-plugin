49b5ed7f9d3c8c5a988eb02400844356b5586ab2
falco442 <marcofra83@gmail.com>
2016-10-04 17:33:42 +0200

    Added authentication

---
+++ plugins/TokenAuth/src/Auth/TokenAuthenticate.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Auth;
+
+use Cake\Auth\FormAuthenticate;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Text;
+
+class TokenAuthenticate extends FormAuthenticate
+{
+    public function getUser(Request $request){
+    	if(!$request->query('token'))
+    		return false;
+    	$table = TableRegistry::get($this->_config['userModel']);
+    	$user = $table->findByToken($request->query('token'))->first();
+    	if(!$user)
+    		return false;
+    	return $user;
+    }
+
+    public function authenticate(Request $request, Response $response){
+    	$user = parent::authenticate($request,$response);
+    	if(!$user){
+    		return $user;
+    	}
+
+    	$table = TableRegistry::get($this->_config['userModel']);
+    	$entity = $table->get($user[$table->primaryKey()]);
+
+    	$entity->token = sha1(Text::uuid());
+    	unset($entity->{$this->_config['fields']['password']});
+
+    	if(!$table->save($entity)){
+	    		$user = false;
+    	}
+    	return $user;
+    }
+
+}
